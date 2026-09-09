2c130d864834c56c66e2faf727b03efc437d0e97
anusha192 <anushasubedi567@gmail.com>
2026-09-09 22:19:24 +0545

    Chart section

--- src/component/Dashboard/Chartsection.jsx
+++ src/component/Dashboard/Chartsection.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
+import RevneuChart from './RevneuChart'
 const Chartsection = () => {
   return (
-    <div className='grid grid-cols-1 xl-grid-cols-3 gqp-6'>
-        <div className=''></div>
+    <div className='grid grid-cols-1 xl:grid-cols-3 gqp-6'>
+        <div className='xl:col-span-2'>
+            <RevneuChart/>
+        </div>
     </div>
   )
 }
--- src/component/Dashboard/Dashboard.jsx
+++ src/component/Dashboard/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import StatsGrid from "./StatsGrid";
+import Chartsection from "./Chartsection";
 
 const Dashboard = () => {
   return (
     <div className="space-y-6">
       <StatsGrid />
+      <Chartsection />
     </div>
   );
 };
